e84ebeb28a0e48afb0ffd522338c0e8ec722cba1
igorVtroi <igorvieiramed@gmail.com>
2026-05-04 22:44:35 -0300

    ola

---
+++ aula_for_ex_5.py
@@ -0,0 +1,19 @@
+# Exercício 5
+import random as rd
+while True:
+    n1 = random.randint(0, 10)
+    n2 = random.randint(0, 10)
+    resultado = n1 * n2
+    print(f'Quanto é {n1} vezes {n2}?')
+    resposta = int(input())
+    if resposta == 0:
+        print("fim do programa")
+        break
+
+    if resultado == resposta:
+        print('ACERTOU!')
+    else:
+        print('ERROU!')
+    continuar = str(input('Continuar? (S/N)\n'))
+    if continuar != 'S' and continuar != 's':
+        break
